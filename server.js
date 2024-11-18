d811450dccfb28dd2ffb6b257b8a5fda2e42056b
athulsabu <athulsabu75@gmail.com>
2024-11-18 16:47:15 +0530

    updated code

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -15,11 +16,12 @@ app.use(cors());
 app.use('/api/todos', todoRoutes);
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/todoDB', {
+mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
+
 
 // Start server
 app.listen(PORT, () => {
